24d08c40061bf28ff55b693723f7c5a69d94f33f
eben <ebenezer.gyamfi@regent.edu.gh>
2023-03-14 04:07:34 +0000

    changed welcome in switch_temp

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -41,7 +41,7 @@
     </div>
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Welcome back") }}
+                    {{ __("Welcome") }}
                 </div>
             </div>
         </div>
